b91f5e996279bf8bd8cf32c3f88c32e71d3a0863
Yao <yaoyao0777@gmail.com>
2017-03-03 17:06:23 +0800

    add: demo

---
+++ src/main/java/singlemsg/Producer.java
@@ -0,0 +1,28 @@
+package singlemsg;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection conn = factory.newConnection();
+        // get a channel
+        Channel channel = conn.createChannel();
+        // declare a queue
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println("Sent " + message + "");
+        // close
+        channel.close();
+        conn.close();
+    }
+}
